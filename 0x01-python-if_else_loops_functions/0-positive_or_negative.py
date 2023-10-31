2a03d00a779dd224016dbc3ed3f6eac2ec96e2eb
Owen Gitau <owengitau02@gmail.com>
2023-10-31 05:07:21 -0700

    Errors

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number < 0:
-    print(f"{number} is Negative")
-elif number > 0:
+if number > 0:
     print(f"{number} is Positive")
+elif number == 0:
+    print(f"{number} is Zero")
 else:
-    print(f"{number} is zero")
+    print(f"{number} is Negative")
