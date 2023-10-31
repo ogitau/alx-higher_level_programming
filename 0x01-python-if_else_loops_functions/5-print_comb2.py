3ed8d4300dc05b78af8c7790ccec3de8833ee44f
Owen Gitau <owengitau02@gmail.com>
2023-10-31 05:41:16 -0700

    fixes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("{:02d}, ".format(x), end="")
+for x in range(0, 100):
+if x == 99:
+    print ("{}".format(x))
+else:
+    print("{:02}, ".format(x), end="")
