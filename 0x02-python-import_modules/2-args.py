fceaeed4a82d3604f30d921d7595bdbd02ca3e59
Owen Gitau <owengitau02@gmail.com>
2023-11-02 03:19:49 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
-    if(args == 0):
+    if args == 0:
         print("0 arguments.")
-    elif(args == 1):
+    elif args == 1:
         print("1 argument.")
     else:
         print(args, "arguments:")
