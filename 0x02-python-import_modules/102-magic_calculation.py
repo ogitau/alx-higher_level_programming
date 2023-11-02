3b6a62883533955c06e82b7a12ffdac449b22b51
Owen Gitau <owengitau02@gmail.com>
2023-11-02 06:15:55 -0700

    Magic Calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -8,3 +8,8 @@ def magic_calculation(a, b):
             return c
         else:
             return sub(a, b)
+
+        if __name__ == "__main__":
+            import dis
+            dis.dis(magic_calculation)
+
