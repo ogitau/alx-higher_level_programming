896ae1a3c0e06eaa324568268118e4f5e68f2bf5
Owen Gitau <owengitau02@gmail.com>
2023-11-04 02:08:25 -0700

    syntax errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    oth = []
-    for x in my_string:
-        if x != 'c' and x != 'C':
-            oth.append(x)
-    return "".join(oth)
+    result = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
