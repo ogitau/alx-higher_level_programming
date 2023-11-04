1c09fb56a84336e6a981297c6b4c9e3e1c8547f3
Owen Gitau <owengitau02@gmail.com>
2023-11-04 03:12:00 -0700

    del at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= 4:
-        cpylist = my_list.copy()
-        return cpylist
+        return my_list
     del my_list[idx]
-    cpylist = my_list.copy()
-    return cpylist
+    return my_list
