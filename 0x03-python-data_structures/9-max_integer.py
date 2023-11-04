5a28b4dd1c15777aa11553158cb790d915c90fb8
Owen Gitau <owengitau02@gmail.com>
2023-11-04 02:23:22 -0700

    fixed error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if(len(my_list) == 0):
-        return(None)
+    if len(my_list) == 0:
+        return None
     else:
-        return(sorted(my_list)[-1])
+        return sorted(my_list)[-1]
