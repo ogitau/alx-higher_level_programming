143b2b224483cd0dada4cd92a9dcb267f3040295
Owen Gitau <owengitau02@gmail.com>
2023-11-08 03:34:38 -0800

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return
-    n = []
-    for new in a_dictionary:
-        n.append(new)
-    return max(n)
+    max_score = None
+    best_key = None
+
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if max_score is None or value > max_score:
+                max_score = value
+                best_key = key
+
+    return best_key
+
