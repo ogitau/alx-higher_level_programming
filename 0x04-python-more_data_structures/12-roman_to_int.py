6726112c5c096d67440451e23138d60fa4af07f0
Owen Gitau <owengitau02@gmail.com>
2023-11-08 06:12:03 -0800

    Roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     if type(roman) != str or len(roman) == 0:
         return 0
     for item in range(item, len(roman)):
-        if item < len(roman) -1 and dic[roman[item]] < dic[roman[item + 1]]:
+        if item < len(roman) - 1 and dic[roman[item]] < dic[roman[item + 1]]:
             result -= dic[roman[item]]
         else:
             result += dic[roman[item]]
