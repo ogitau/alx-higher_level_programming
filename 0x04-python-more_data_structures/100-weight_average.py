8ef883482ff64cb43a220ed0050a40ab51222b71
Owen Gitau <owengitau02@gmail.com>
2023-11-08 07:14:11 -0800

    corrections

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0 or my_list is None:
-        return 0
-    sum_a = sum_b = product = average = 0
-    for tuple in my_list:
-        sum_a += tuple[1]
-        product = tuple[0] * tuple[1]
-        sumb += product
-        average = sum_b / sum_a
-    return average
+    if(my_list):
+        return (sum(x*y for x, y in my_list) / sum(y for x, y in my_list))
+    else:
+        return (0)
