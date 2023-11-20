b3caa52e3c6c1a5a99800f5ccbb04bdf7c0c7ed0
Owen Gitau <owengitau02@gmail.com>
2023-11-20 05:38:16 -0800

    Safe Function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except(IndexError, ValueError, NameError):
+    except(TypeError, ZeroDivisionError, IndexError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
