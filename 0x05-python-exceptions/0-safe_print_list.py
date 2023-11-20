f6b5d6f5ed3aaddda835831364f3e3730a9cfba9
Owen Gitau <owengitau02@gmail.com>
2023-11-20 01:48:59 -0800

    pycodestyle Errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print ("{}".format(my_list[i]), end ="")
+            print("{}".format(my_list[i]), end="")
             index += 1
     except IndexError:
         pass
     finally:
-       print()
-       return index
+        print()
+        return index
