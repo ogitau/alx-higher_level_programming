571fe058d38eb4c739fd8b09d705a19c44921a33
Owen Gitau <owengitau02@gmail.com>
2023-11-21 02:24:00 -0800

    corrections

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
+"""initializes a class square"""
+
+
 class Square:
+
+    """creates a class square"""
+
     def __init__(self, size=0):
+
+        """initalizes attributes of square"""
+
         if (type(size) is not int):
             raise TypeError("size must be an integer")
+
+        """checks if condition is not int is met and raises error if not"""
+
         if (size < 0):
             raise ValueError("size must be >= 0")
+
+        """checks if condition > 0 is met and raises error if not"""
+
         self.__size = size
-        """
-        initializes size as an integer
-        checks if size is of type int and is > 0
-        raises errors if conditions not met
-        """
+        """initializes size as an integer
+        checks for several conditions and raises errors if not met"""
+
         def area(self):
             return self.__size * self.__size
-        """
-        define area and returns it based on size
-        """
+
+        """finds the area and returns it """
