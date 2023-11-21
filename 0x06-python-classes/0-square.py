2d99eb0267872bb5937510973a90523be2d503fe
Owen Gitau <owengitau02@gmail.com>
2023-11-21 01:24:26 -0800

    empty square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ defines a class Square"""
+
+
 class Square:
+
     """class Square"""
+
     pass
-"""
-creates an empty class Square
-"""
+
+    """creates an empty class Square"""
