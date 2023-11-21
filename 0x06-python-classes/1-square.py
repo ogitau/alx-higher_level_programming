46cca1766dddcac017143a758e29ce0feec92bff
Owen Gitau <owengitau02@gmail.com>
2023-11-20 23:43:40 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """
+    creates a class Square
+    """
+    def __init__ (self, size):
+        self.__size = size
+        """
+        initialized class object square
+        initialized a private attribute:
+        __size(int)
+        """
