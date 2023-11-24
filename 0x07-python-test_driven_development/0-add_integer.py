c378eda4a86c3ce0f5bee4b4834bb3140ab79739
Owen Gitau <owengitau02@gmail.com>
2023-11-24 14:53:19 -0800

    Say My Name

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""Addition module to add two integers"""
+"""Addition module to add two integers
+checks if 'a' is an integer or a float
+checks if 'b' is an integer or a float
+
+Return a + b"""
 
 
 def add_integer(a, b=98):
+    
     """Return a + b"""
 
     if not isinstance(a, (int, float)):
+
+        """checks if 'a' is an integer or a float"""
+
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
+        
+        """checks if 'b' is an integer or a float"""
+        
         raise TypeError("b must be an integer")
     return int(a) + int(b)
+
