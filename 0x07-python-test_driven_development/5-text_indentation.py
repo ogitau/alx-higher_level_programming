656bfb4b132a0d46dbcc220cc86b395cfa2d8522
Owen Gitau <owengitau02@gmail.com>
2023-11-28 12:40:03 -0800

    unittest

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""My string module performs operations on strings
+"""
+
+
+def text_indentation(text):
+    """Prints 2 new lines after each . ? or : character in a string"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    operators = ['.', '?', ':']
+    new_str = ""
+    for letter in text:
+        new_str += letter
+        if letter in operators:
+            new_str += "\n"
+            print(new_str.strip(" "))
+            new_str = ""
+    if letter not in operators:
+        print(new_str.strip(" "), end="")
