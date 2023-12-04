805d8dd133be7311973b9669377dff19d8b7d768
Owen Gitau <owengitau02@gmail.com>
2023-12-04 04:52:50 -0800

    Integer Validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""instantiation of Module for geometry"""
+
+
+class BaseGeometry():
+    """the Base class"""
+    def area(self):
+        """ Method that raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Method that checks if value is a number and 
+        if it's an appropriate number"""
+
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
