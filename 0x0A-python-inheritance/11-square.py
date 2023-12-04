8eaea72d020146912236dee17303f1dcb3ff5cd0
Owen Gitau <owengitau02@gmail.com>
2023-12-04 06:19:53 -0800

    square#2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" My Square module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square inherits from class rectanglr"""
+    def __init__(self, size):
+        """initialization of size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """returns the string representation of the square"""
+        return "[square] {}/{}".format(self.__size, self.__size)
