86d559af35474bcfbbfa6122fc8d02e6d5f0a21e
Owen Gitau <owengitau02@gmail.com>
2023-12-04 01:36:30 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""instanstiation of a function that that returns the list of available attributes and methods of an object"""
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+
+    return dir(obj)
