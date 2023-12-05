54db7ce2aa1090edc42640998393aa34dd480d7d
Owen Gitau <owengitau02@gmail.com>
2023-12-05 02:23:42 -0800

    from JSON

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,5 +4,5 @@ import json
 
 
 def to_json_string(my_obj):
-   """converts obj to a JSON string"""
-   return json.dumps(my_obj)
+    """converts obj to a JSON string"""
+    return json.dumps(my_obj)
