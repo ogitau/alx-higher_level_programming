0ea02f5698b3af1193458e762eae1ec802331e08
Owen Gitau <owengitau02@gmail.com>
2023-12-05 01:41:14 -0800

    write file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""function that reads a text file (UTF8) and prints it to stdout"""
+"""reads a file"""
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
-
-    with open(filename, encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """reads a file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""function that writes a string to a text file (UTF8) and
-returns the number of characters written"""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """returns the number of characters written"""
-
-    with open(filename, "w", encoding="UTF-8") as f:
+    """Returns:The number of characters written."""
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
