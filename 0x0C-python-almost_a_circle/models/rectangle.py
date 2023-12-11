54fa36a29bff211c81d8d8020405308a90454428
Owen Gitau <owengitau02@gmail.com>
2023-12-11 09:10:03 -0800

    damn you error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,8 +23,9 @@ class Rectangle(Base):
 
     def __str__(self):
         """string representation of rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
-                                                        self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
 
     def update(self, *args, **kwargs):
         """updates for attributes of rectangle"""
