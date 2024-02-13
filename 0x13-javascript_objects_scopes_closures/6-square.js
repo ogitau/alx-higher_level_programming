9c6fc15a1d13df323bb7e36df78b174fcffe2086
Owen Gitau <owengitau02@gmail.com>
2024-02-13 03:33:02 -0800

    convertor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,11 @@
 const Square5 = require('./5-square');
 
 class Square extends Square5 {
-	charPrint (c) {
-		if (c === undefined) c = 'X';
-		for (let i = 0; i = < this.height; i++) {
-			console.log(c.repeat(this.width));
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
-module.exports = Square
+module.exports = Square;
