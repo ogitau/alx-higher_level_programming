2d792b570e77751847bce7f88d9e9eddefcf67b6
Owen Gitau <owengitau02@gmail.com>
2024-02-16 01:49:08 -0800

    fixed errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                        user=argv[1], passwd=argv[2], db=argv[3])
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
                  FROM cities \
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                  ON cities.state_id = states.id \
                  ORDER BY cities.id ASC")
     query = cur.fetchall()
-    
+
     for row in query:
         print(row)
     cur.close()
