19d5e94933db180f61f65b03ca898751ee7e2a25
Owen Gitau <owengitau02@gmail.com>
2024-02-29 06:11:08 -0800

    Catch me if you can!

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to send JSON POST request to url passed as frst arg and show response
-curl -s -X POST -H "Content_Type: application/json" -d @"${2}" "${1}"
+curl -s -X POST -H "Content-Type: application/json" -d @"${2}" "${1}"
