fe222227affa4ff8d8786ed938868ada37dedfb2
Owen Gitau <owengitau02@gmail.com>
2024-02-29 02:17:18 -0800

    Methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#  script that takes in a URL and displays all
-#  HTTP methods the server will accept.
-
+#  script that takes in a URL and displays all allowed HTTP methods
 curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
