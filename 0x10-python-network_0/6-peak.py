cf2f0aca9f9d0c4dd06bfebe22cbbfb1246c15a2
Owen Gitau <owengitau02@gmail.com>
2024-02-29 06:37:23 -0800

    corrections

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Algorithms containing a list of integers."""
+"""Finds a peak in a list of unsorted integers."""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
+    """ contains a list of integers"""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
