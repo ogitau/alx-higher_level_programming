51136be8c03ee7ed6117757071233e6c905d3fad
Owen <owengitau02@gmail.com>
2024-03-26 14:41:47 -0700

    Starwars Characters

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+	if (err) console.log(err);
+	else {
+		const donetasks = {};
+		const todos = JSON.parse(body);
+
+		for (const task in todos) {
+			if (todos[task].completed === true) {
+				if (donetasks[todos[task].userId]) donetasks[todos[task].userId]++;
+				else donetasks[todos[task].userId] = 1;
+			}
+		}
+		console.log(donetasks);
+	}
+});
